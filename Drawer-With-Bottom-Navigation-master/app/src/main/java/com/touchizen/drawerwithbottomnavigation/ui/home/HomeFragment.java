393fbc7ba9f12bdd1708a2875b31c17108bd8b21
Pablo Ernesto Cortes Palma <tecnoplacita@gmail.com>
2024-09-18 22:46:21 -0600

    fix ui experiencia de usuario

--- Drawer-With-Bottom-Navigation-master/app/src/main/java/com/touchizen/drawerwithbottomnavigation/ui/home/HomeFragment.java
+++ Drawer-With-Bottom-Navigation-master/app/src/main/java/com/touchizen/drawerwithbottomnavigation/ui/home/HomeFragment.java
@@ -1,120 +1,77 @@
+// com/touchizen/drawerwithbottomnavigation/ui/home/HomeFragment.java
 package com.touchizen.drawerwithbottomnavigation.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.touchizen.drawerwithbottomnavigation.R;
-import com.touchizen.drawerwithbottomnavigation.io.NetworkApiAdapter;
-import com.touchizen.drawerwithbottomnavigation.io.responses.ClienteResponse;
-import com.touchizen.drawerwithbottomnavigation.io.responses.UsuariosReponse;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+public class HomeFragment extends Fragment {
 
-public class HomeFragment extends Fragment implements Callback<UsuariosReponse> {
-    ListViewAdapterHome adapter;
-    String[] titulo = new String[]{
-            "Adelanto de quincena",
-            "Bitcoin  Comprados"
-    };
-
-    String[] mensaje = new String[]{
-            "Actualmente estas al Corriente",
-            "Actualmente tienes 1 Bitcoin"
-    };
-
-    int[] imagenes = {
-            R.drawable.baseline_paid_24,
-            R.drawable.baseline_paid_24
-
-
-    };
-
-    private HomeViewModel homeViewModel;
-    TextView textcredito=null;
-    TextView txtsaldo=null;
-    TextView textCliente=null;
-    TextView textEmail=null;
-    TextView textActivate=null;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        obtenerDatosClientes();
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-         textCliente=root.findViewById(R.id.text_nombre);
-         textcredito=root.findViewById(R.id.text_credito_disponible);
-         txtsaldo=root.findViewById(R.id.text_Saldo);
-        textEmail=root.findViewById(R.id.text_Email);
-        textActivate=root.findViewById(R.id.text_Activate);
-
-        final ListView lista = (ListView) root.findViewById(R.id.listViewHome);
-        adapter = new ListViewAdapterHome(this, titulo, mensaje,imagenes,inflater);
-        lista.setAdapter(adapter);
-
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
-                Toast.makeText(getActivity(), "presiono " + i, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView adapterView, View view, int i, long l) {
-                Toast.makeText(getActivity(), "presiono LARGO " + i, Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-        return root;
-    }
-
-    private void obtenerDatosClientes() {
-        String idCliente = "1";
-        Call<UsuariosReponse> call= NetworkApiAdapter.getApiService().getUsuarios(idCliente);
-        call.enqueue(this);
-    }
-
+    private TextView verbTitle;
+    private TextView conjugationsContent;
+    private TextView pronunciationContent;
+    private TextView comparativesContent;
+    private TextView pronounsContent;
+    private TextView adverbsPrepositionsContent;
+    private TextView connectorsContent;
+    private TextView conversationContent;
+    private Button exampleButton;
+    private Button audioButton;
+    private ImageButton backButton;
 
     @Override
-    public void onResponse(Call<UsuariosReponse> call, Response<UsuariosReponse> response) {
-        if(response.isSuccessful()){
-            UsuariosReponse respuestaServicioCliente=response.body();
-            poblarPerfil(response.body());
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize UI components
+        verbTitle = view.findViewById(R.id.verbTitle);
+        conjugationsContent = view.findViewById(R.id.conjugationsContent);
+        pronunciationContent = view.findViewById(R.id.pronunciationContent);
+        comparativesContent = view.findViewById(R.id.comparativesContent);
+        pronounsContent = view.findViewById(R.id.pronounsContent);
+        adverbsPrepositionsContent = view.findViewById(R.id.adverbsPrepositionsContent);
+        connectorsContent = view.findViewById(R.id.connectorsContent);
+        conversationContent = view.findViewById(R.id.conversationContent);
+        exampleButton = view.findViewById(R.id.exampleButton);
+        audioButton = view.findViewById(R.id.audioButton);
+        backButton = view.findViewById(R.id.backButton);
+
+        // Set data (this could be replaced with dynamic data fetching)
+        setVerbData();
+
+        // Set click listeners
+        exampleButton.setOnClickListener(v -> showExampleUsage());
+        audioButton.setOnClickListener(v -> playAudio());
+        backButton.setOnClickListener(v -> getActivity().onBackPressed());
+
+        return view;
     }
 
-    private void poblarPerfil(UsuariosReponse body) {
-        textCliente.setText("Bienvenida a mi Bolsillo: " +body.getFirstName() +" " +body.getLastName());
-        textcredito.setText("Credito con el que cuentas $10000");
-        textEmail.setText("Correo Registrado: "+body.getEmail());
-        if(body.isActivated()==true){
-        textActivate.setText("Estatus: Activo");
-        }else{
-            textActivate.setText("Estatus: Inactivo");
-        }
-        txtsaldo.setText("Productos con el que se cuenta ");
-
-//        textSaldoView.setText("30000");
-//        textCreditoDisponibleView.setText("90000");
+    private void setVerbData() {
+        // This is where you set the data for the verb. For example:
+        verbTitle.setText("To Speak");
+        conjugationsContent.setText("I speak, you spoke, he will speak");
+        pronunciationContent.setText("/spiːk/");
+        comparativesContent.setText("Better, Best");
+        pronounsContent.setText("My, your, himself, etc.");
+        adverbsPrepositionsContent.setText("Quickly, on");
+        connectorsContent.setText("However, therefore");
+        conversationContent.setText("A: How often do you speak English?\nB: I speak English every day.");
     }
 
+    private void showExampleUsage() {
+        // Implement functionality to show example usage
+    }
 
-    @Override
-    public void onFailure(Call<UsuariosReponse> call, Throwable t) {
-
+    private void playAudio() {
+        // Implement functionality to play audio
     }
 }
